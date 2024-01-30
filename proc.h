b1838897a2c747c530df5b7fb7417eb8b914d6a9
ArminAfzali <armin11dse@gmail.com>
2024-01-30 14:16:37 +0330

    removed unecessary lock

--- proc.h
+++ proc.h
@@ -52,7 +52,6 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int tid;
-  struct lock lk;
 };
 
 // Process memory is laid out contiguously, low addresses first:
